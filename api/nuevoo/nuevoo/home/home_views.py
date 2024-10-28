76ebe4302986d399a3d99e7a5c390991c9a8f0de
chim-py <erick.puebla55@gmail.com>
2024-10-27 21:41:43 -0600

    Mensaje del commit describiendo los cambios

---
+++ api/nuevoo/nuevoo/home/home_views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from api.home.value_const import LOGING_URL
+
+
+
+@login_required(login_url=LOGING_URL)
+def home_views(request):
+    template_name = "index.html"
+    
+    return render(request, template_name)
+
